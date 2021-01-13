8878c6dd3b5c2709998dcc9ce9be6be9adab914f
Rolf Johansson <rolf.johansson@chalmers.se>
2021-01-13 22:18:52 +0100

    Some initial tests of parsing the QTI XML.

--- src/main.py
+++ src/main.py
@@ -9,12 +9,38 @@ __license__ = "Apache"
 
 import argparse
 from logzero import logger
+from lxml import etree
+
+qti_resource = {}
 
 def main(args):
     """ Main entry point of the app """
     logger.info("Main program starting point.")
     logger.info(args)
 
+    xml_doc = etree.parse(args.filename)
+
+    for xml_resource in xml_doc.getroot().findall(".//{http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1}resource[@type='imsqti_xmlv1p2']"):
+        this_resource_id = xml_resource.get("identifier")
+        qti_resource[this_resource_id] = {}
+        qti_resource[this_resource_id]['id'] = this_resource_id
+        qti_resource[this_resource_id]['href'] = xml_resource.find("{http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1}file").get("href")
+        qti_resource[this_resource_id]['title'] = etree.parse(qti_resource[xml_resource.get("identifier")]['href']).getroot().find("{http://www.imsglobal.org/xsd/ims_qtiasiv1p2}assessment").get("title")
+        qti_resource[this_resource_id]['items'] = {}
+
+        for xml_item in etree.parse(qti_resource[xml_resource.get("identifier")]['href']).getroot().findall(".//{http://www.imsglobal.org/xsd/ims_qtiasiv1p2}item"):
+            this_item_id = xml_item.get("ident")
+            qti_resource[this_resource_id]['items'][this_item_id] = {}
+            qti_resource[this_resource_id]['items'][this_item_id]['id'] = this_item_id
+            qti_resource[this_resource_id]['items'][this_item_id]['title'] = xml_item.find("{http://www.imsglobal.org/xsd/ims_qtiasiv1p2}presentation/{http://www.imsglobal.org/xsd/ims_qtiasiv1p2}material/{http://www.imsglobal.org/xsd/ims_qtiasiv1p2}mattext").text
+
+    for assessment in qti_resource:
+        a = qti_resource[assessment]
+        print(a['id'] + " " + a['title'])
+        for item in a['items']:
+            i = a['items'][item]
+            print(i['id'] + i['title'])
+    
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
---
+++ src/parser/imsmanifest.py
@@ -0,0 +1,1 @@
+# http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1
